e58399c46b028e9009e5d17ae179023796d8952a
Gabriela Álvarez <61790728+gabyjazzmin@users.noreply.github.com>
2020-04-24 21:56:31 -0500

    Update with method "muestra"

--- Reloj.h
+++ Reloj.h
@@ -1,44 +1,43 @@
 #include <iostream>
+#include <iomanip>
 
-using namespace std;
+//using namespace std;
 
-class Reloj {
-public:
+class Reloj{
+    public:
 
     Reloj();
     //constructor default
 
-    Reloj(int x, y);
+    Reloj(int x, int y);
     //constructor con parametros
 
-    void setHora(int h) { hora = ((h >= 0 && h <= 24) ? h : 0); };
-    void setMin(int m) {
-        min = ((m >= 0 && m <= 59) ? m : 0););
-        int getHora() { return hora; };
-        int getMin() { return min; };
-        //metodos de acceso set y get
+    void setHora(int h) {hora = ((h>=0 && h <24)?h:0);};
+    void setMin(int m) {min =((m>=0 && m<=59) ?m:0);};
+    int getHora() {return hora; };
+    int getMin() {return min; };
+    //metodos de acceso set y get
 
-        void muestra();
-        //metodo muestra
-private:
+    void muestra();
+    //metodo muestra
+    private:
 
     int hora, min;
 
-    };
+};
 
-    Reloj::Reloj() {
-        hora = 0;
-        min = 0;
-    }
-
-    Reloj::Reloj(x, y) {
-        hora = x;
-        min = y;
-    }
-
-    void Reloj::muestra() {
-        cout << getHora() << ":" << getMin() << endl;
-    }
-
-  return 0;
+Reloj::Reloj(){
+  hora = 0;
+  min = 0;
 }
+
+Reloj :: Reloj(int x,int y){
+  hora = x;
+  min = y;
+}
+void Reloj::muestra()
+{
+	std::cout << std::setw(2) << std::setfill('0') << hora << ":"
+	<< std::setw(2) << std::setfill('0') << min;
+}
+
